5c8b9563238b48eb673db0e52acca4d2b3ba0d07
hebei5920 <hebei5920@gmail.com>
2025-05-16 13:55:20 +0800

    修改默认主题

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,7 +6,7 @@ import { AuthProvider } from '@/lib/auth-context';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
       <LanguageProvider>
         <AuthProvider>
           {children}
